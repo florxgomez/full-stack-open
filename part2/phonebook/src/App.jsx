a2c852a9d5c1e482789477755d3e5f0358423c15
Florencia Gomez <florxgomez@gmail.com>
2023-08-29 20:16:28 -0300

    exercise 2.12 - 2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import numbersService from './services/numbers';
 
 const SearchFilter = ({ search, onSearch }) => {
   return (
@@ -10,7 +10,7 @@ const SearchFilter = ({ search, onSearch }) => {
   );
 };
 
-const PersonForm = ({
+const NumberForm = ({
   onSubmit,
   newName,
   newNumber,
@@ -32,59 +32,88 @@ const PersonForm = ({
   );
 };
 
-const Person = ({ person }) => {
+const Number = ({ number }) => {
   return (
-    <p>
-      {person.name} {person.number}
-    </p>
+    <span>
+      {number.name} {number.number}{' '}
+    </span>
   );
 };
 
-const Persons = ({ persons }) => {
+const Numbers = ({ numbers, onNumberDelete }) => {
   return (
     <>
-      {persons.map((person) => (
-        <Person person={person} key={person.id} />
+      {numbers.map((number) => (
+        <div key={number.id}>
+          <Number number={number} />
+          <button onClick={() => onNumberDelete(number.id)}>X</button>
+        </div>
       ))}
     </>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [numbers, setNumbers] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then((response) => {
-      setPersons(response.data);
+    numbersService.getAll().then((data) => {
+      setNumbers(data);
     });
   }, []);
 
+  function handleNumberDelete(id) {
+    const numberToDelete = numbers.find((number) => number.id === id);
+    if (window.confirm(`Delete ${numberToDelete.name}?`)) {
+      numbersService.deleteNumber(id).then((response) => {
+        if (response === 'OK') {
+          setNumbers(numbers.filter((number) => number.id !== id));
+        }
+      });
+    }
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
-    const newPerson = {
+    const newObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
+      id: numbers.length + 1,
     };
 
-    const sameName = persons.filter((person) =>
-      person.name.toLowerCase().startsWith(newName.toLowerCase())
+    const sameName = numbers.find((number) =>
+      number.name.toLowerCase().startsWith(newName.toLowerCase())
     );
 
-    if (sameName.length > 0) {
-      alert(`${newName} is already added to phonebook`);
+    if (sameName) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        numbersService
+          .updateNumber(sameName.id, { ...newObject, id: sameName.id })
+          .then((response) => {
+            setNumbers(
+              numbers.map((number) =>
+                number.id !== sameName.id ? number : response
+              )
+            );
+          });
+      }
     } else {
-      setPersons(persons.concat(newPerson));
+      numbersService.createNumber(newObject).then((data) => {
+        setNumbers(numbers.concat(data));
+      });
     }
     setNewName('');
     setNewNumber('');
   }
-
-  const personsToShow = persons.filter((person) =>
-    person.name.toLowerCase().includes(search.toLowerCase())
+  const numbersToShow = numbers.filter((number) =>
+    number.name.toLowerCase().includes(search.toLowerCase())
   );
 
   function handleSearch(e) {
@@ -104,7 +133,7 @@ const App = () => {
       <h1>Phonebook</h1>
       <SearchFilter search={search} onSearch={handleSearch} />
       <h2>add a new</h2>
-      <PersonForm
+      <NumberForm
         onSubmit={handleSubmit}
         newName={newName}
         newNumber={newNumber}
@@ -112,7 +141,7 @@ const App = () => {
         onNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons persons={personsToShow} />
+      <Numbers numbers={numbersToShow} onNumberDelete={handleNumberDelete} />
     </div>
   );
 };
---
+++ part2/phonebook/src/services/numbers.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/numbers';
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const createNumber = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const updateNumber = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+const deleteNumber = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.statusText);
+};
+
+export default { getAll, createNumber, deleteNumber, updateNumber };
